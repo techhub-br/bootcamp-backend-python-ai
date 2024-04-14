564bc8b6567b78fdfcb0d9bbf7472e23414f83e9
Valdir Amaral <109113918+valdir-amaral@users.noreply.github.com>
2024-04-14 18:11:04 -0300

    Add multi line strings lesson

---
+++ 04-dominando-strings/4-string-multi-linhas/ex.py
@@ -0,0 +1,14 @@
+# String com múltiplas linhas (ou strings triplas)
+
+# São definidas informando 3 aspas simples ou duplas durante a atribuição.
+
+nome = "Roberto"
+
+mensagem = f"""
+Olá, meu nome é {nome},
+   Eu estou aprendendo Python!
+"""
+
+print(mensagem)
+# Olá, meu nome é Roberto,
+#    Eu estou aprendendo Python!
